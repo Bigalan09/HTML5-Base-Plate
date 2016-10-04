aaaec9ced7081d4b656ead4008733610369a2f4b
Bigalan09 <bigalan09@gmail.com>
2016-10-04 10:13:08 +0100

    remove everything for latest

--- README.md
+++
@@ -1,5 +0,0 @@
-HTML5-Base-Plate
-================
-
-A HTML5 base website layout.
-Makes use of https://github.com/h5bp/html5-boilerplate
--- js/main.js
+++
@@ -1,9 +0,0 @@
-$('nav li ul').hide().removeClass('fallback');
-$('nav li').hover(
-    function () {
-        $('ul', this).stop().slideDown(150);
-    },
-    function () {
-        $('ul', this).stop().slideUp(200);
-    }
-);
